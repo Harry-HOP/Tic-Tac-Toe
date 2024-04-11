1a464edaeb03991e0a569b42f677c81d75586d72
hariom <hariompoddar123@gmail.com>
2024-04-11 13:33:49 +0530

    First change

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 var boxes = document.getElementsByClassName("box");
 let n = document.querySelector(".box");
+var turn = true;
 
 let arr = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[1,4,8],[2,4,6]];
 
 for(let i=0; i<boxes.length; i++){
     boxes[i].addEventListener("click" , () => {
-        if(boxes[i].innerText===""){
+        if(turn){
             boxes[i].innerText = "X";
+            turn = false;
         }
         else{
-            boxes[i].innerText = "";
+            boxes[i].innerText = "O";
+            turn = true;
         }
     })
 }
